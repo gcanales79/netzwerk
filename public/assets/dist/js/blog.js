1c2e5f37b13d25886f599b454ac7320425b74809
Gus <gustavo.canales@me.com>
2021-03-16 19:02:25 +0100

    added facebook plugin to blog post

---
+++ public/assets/dist/js/blog.js
@@ -0,0 +1,4 @@
+$(document).ready(function (){
+    
+    $("#facebookFrame").attr("src","https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fnetzwerkmx&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId=212194213188545")
+})
